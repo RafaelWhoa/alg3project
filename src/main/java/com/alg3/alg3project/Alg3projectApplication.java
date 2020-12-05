fdb2ba5f7b8c00f474309079cbb291d5d8ca4bd7
Rafael Whoa <rafaelwhoa@gmail.com>
2020-12-04 23:08:15 -0400

    Initial commit

---
+++ src/main/java/com/alg3/alg3project/Alg3projectApplication.java
@@ -0,0 +1,13 @@
+package com.alg3.alg3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alg3projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Alg3projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alg3/alg3project/Alg3projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alg3.alg3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alg3projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
